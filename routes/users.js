723d3e831039b09f66e0b3ddce6895a1fad7dd5b
jay <dyjay15@gmail.com>
2020-09-08 12:57:25 +0800

    add validation to the fields of the users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
+const User = require('../models/User');
 // @route   POST  api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please Enter a Name').not().isEmpty(),
+    check('email', 'Please Enter a Valid Email').isEmail(),
+    check('password', 'Please Enter more than 6 Character').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // res.send(req.body);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
