384c0ca15bdc5624df65cf6962aa8a2ee4eddd2d
jay <dyjay15@gmail.com>
2020-09-09 09:23:46 +0800

    make contact model for the application

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const { Mongoose } = require('mongoose');
+
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'psersonal',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
