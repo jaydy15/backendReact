a3063b0070075aab264ec18cd3d1d59c06ce733f
jay <dyjay15@gmail.com>
2020-09-10 13:01:28 +0800

    setup file for initial state of the auth and make the reducer and context of auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/layout/Home';
 import About from './components/layout/About';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 };
 
